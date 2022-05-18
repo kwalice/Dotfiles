f75f75499de241dababe557bbe0bccb8f42704a1
kwalice <kwalice6@gmail.com>
2022-05-18 20:49:39 -0300

    my fisrt commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+mv alacritty ~/.config
+
+mv bspwm ~/.config
+
+mv dunst ~/.config
+
+mv fish ~/.config
+
+mv picom ~/.config
+
+mv polybar ~/.config
+
+mv spotifyd ~/.config
+
+mv spotify-tui ~/.config
+
+mv sxhkd ~/.config
+
